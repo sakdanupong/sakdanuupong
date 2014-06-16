368f130ccbb312715eb52b469f0a9d76a19937ae
Sakdanupong <sakdanupong.v@gmail.com>
2014-06-17 02:14:34 +0700

    work in device image

--- main.py
+++ main.py
@@ -14,11 +14,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+import urllib
+import jinja2
 import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+    	template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
